8c0d313fbd703a939c5b13f2cf7c2e0373da4fba
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2020-07-15 17:25:24 +0700

    Add files via upload

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -32,3 +32,13 @@ exports.loginFacebook = catchAsync(async(req,res,next)=>{
     const token = await user.generateToken()
     res.json({status:"ok",data:user,token})
 })
+
+exports.logout = (req, res) => {
+    req.logout();
+    req.session.destroy((err) => {
+      if (err) console.log('Error : Failed to destroy the session during logout.', err);
+      req.user = null;
+      res.redirect('/');
+    });
+  };
+
